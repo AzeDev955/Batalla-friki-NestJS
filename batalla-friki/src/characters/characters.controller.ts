f9755a8b6c2a33811ef6a60329d4ba5c473a9cd6
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2026-02-15 18:32:14 +0100

    roles y controller de characters

--- batalla-friki/src/characters/characters.controller.ts
+++ batalla-friki/src/characters/characters.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CharactersService } from './characters.service';
 import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
 
+import { JwtAuthGuard } from 'src/auth/jwt.strategy/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { Roles } from 'src/auth/roles/roles.decorator';
+
 @Controller('characters')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class CharactersController {
   constructor(private readonly charactersService: CharactersService) {}
 
   @Post()
+  @Roles('ADMIN')
+  @UseGuards(JwtAuthGuard, RolesGuard)
   create(@Body() createCharacterDto: CreateCharacterDto) {
     return this.charactersService.create(createCharacterDto);
   }
@@ -18,17 +35,24 @@ export class CharactersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.charactersService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.charactersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCharacterDto: UpdateCharacterDto) {
-    return this.charactersService.update(+id, updateCharacterDto);
+  @Roles('ADMIN') // Solo Admin puede editar
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCharacterDto: UpdateCharacterDto,
+  ) {
+    return this.charactersService.update(id, updateCharacterDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.charactersService.remove(+id);
+  @Roles('ADMIN') // Solo Admin puede borrar
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.charactersService.remove(id);
   }
 }
