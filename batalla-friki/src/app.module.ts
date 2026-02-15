9534de46d90dba2ee0f5e52dcee3e663e647cf9a
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2026-02-15 12:55:20 +0100

    prisma service y character dto

--- batalla-friki/src/app.module.ts
+++ batalla-friki/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { CharactersModule } from './characters/characters.module';
 import { BattlesModule } from './battles/battles.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CharactersModule, BattlesModule],
+  imports: [AuthModule, UsersModule, CharactersModule, BattlesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- batalla-friki/src/characters/dto/create-character.dto.ts
+++ batalla-friki/src/characters/dto/create-character.dto.ts
@@ -1,1 +1,31 @@
-export class CreateCharacterDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  Min,
+  IsUrl,
+} from 'class-validator';
+
+export class CreateCharacterDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @Min(1)
+  hp: number; // Puntos de vida
+
+  @IsInt()
+  @Min(0)
+  attack: number; // Puntos de ataque
+
+  @IsInt()
+  @Min(1)
+  level: number; // Nivel de fuerza del monstruo
+
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  minLevel?: number;
+}
