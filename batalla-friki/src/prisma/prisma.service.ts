626aefd8f6ddb5fd5673fbc9ab374de70222e6ba
AlvaroDaw2024 <alvarodaw2024@gmail.com>
2026-02-17 15:21:41 +0100

    commit
    parece que ya?!

--- batalla-friki/src/prisma/prisma.service.ts
+++ batalla-friki/src/prisma/prisma.service.ts
@@ -1,19 +1,27 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import pg from 'pg';
+import { join } from 'node:path';
+
+const { PrismaClient } = require(join(process.cwd(), 'generated/prisma2'));
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    const connectionString = `${process.env.DATABASE_URL}`;
-    const pool = new pg.Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-
-    super({ adapter });
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+      }),
+    });
   }
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
